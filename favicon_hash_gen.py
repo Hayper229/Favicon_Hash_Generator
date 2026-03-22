e2b13f0090f307d8a7713db5bef99cee900be7fa
miko <105974108+Hayper229@users.noreply.github.com>
2026-03-22 16:42:49 +0300

    Update favicon_hash_gen.py

--- favicon_hash_gen.py
+++ favicon_hash_gen.py
@@ -12,14 +12,10 @@ def main():
             pass
        b64 = base64.encodebytes(data)
        favicon_hash = mmh3.hash(b64)
-       print(f'{colorama.Fore.WHITE}[{colorama.Fore.BLUE}{time.asctime()}{colorama.Fore.WHITE}] {colorama.Fore.LIGHTGREEN_EX}Получен Хеш FavIcon') if favicon_hash is not None el>
-       print(f'{colorama.Fore.WHITE}[{colorama.Fore.BLUE}{time.asctime()}{colorama.Fore.WHITE}] {colorama.Fore.LIGHTGREEN_EX}Hash{colorama.Fore.RED}: {colorama.Fore.YELLOW}{favi>
+       print(f'{colorama.Fore.WHITE}[{colorama.Fore.BLUE}{time.asctime()}{colorama.Fore.WHITE}] {colorama.Fore.LIGHTGREEN_EX}Получен Хеш FavIcon') if favicon_hash is not None else print(f'{colorama.Fore.RED}Ошибка генерации')
+       print(f'{colorama.Fore.WHITE}[{colorama.Fore.BLUE}{time.asctime()}{colorama.Fore.WHITE}] {colorama.Fore.LIGHTGREEN_EX}Hash{colorama.Fore.RED}: {colorama.Fore.YELLOW}{favicon_hash}{colorama.Fore.RESET}')
     except FileNotFoundError:
            print(f'file not found')
 
 if __name__ == "__main__":
    main()
-
-
-
-
